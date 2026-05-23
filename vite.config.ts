cf289a6eb2718eb7d6d384f03ae7942ef4b38620
lenilsonxavier-dev <lenilsonxavier@gmail.com>
2026-05-23 20:25:46 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,9 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
 
-import { defineConfig } from 'vite'
-
 export default defineConfig({
-  base: '/Quintin-Ai/'
-})
+  base: '/Quintin-Ai/',
+
   plugins: [
     react(),
     tailwindcss()
